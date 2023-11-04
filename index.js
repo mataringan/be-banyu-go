0dfe060c25d5082cf5d559a44cbf8369e469db27
mataringan <taofikarianto@gmail.com>
2023-11-04 22:14:22 +0700

    feat: auth

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const apiRouters = require("./src/routes/index");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+
 app.use("/", apiRouters);
 mongoose.set("strictQuery", false);
 mongoose
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const { handleRoot } = require("../controllers/root");
+const {
+    register,
+    registerAdmin,
+    verifyUser,
+    resendOTP,
+    login,
+    whoami,
+} = require("../controllers/authController");
+const { authorize } = require("../middleware/authorize");
 
 const router = express.Router();
 
 router.get("/", handleRoot);
+
+router.post("/register", register);
+
+router.post("/register-admin", authorize, registerAdmin);
+
+router.put("/verify-user", verifyUser);
+
+router.post("/resend-otp", resendOTP);
+
+router.post("/login", login);
+
+router.get("/whoami", authorize, whoami);
 
 module.exports = router;
